7c123fee2324a5f636468222f8e85658fa9bf21e
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2023-09-26 08:22:00 -0700

    add login modal

--- js/app.js
+++ js/app.js
@@ -1,6 +1,20 @@
+// Menu toggle
 const toggleButton = document.querySelector('.hamburger-icon')
 const menu = document.getElementById('menu')
 
 toggleButton.addEventListener('click', () => {
   menu.classList.toggle('hidden')
+})
+
+// Modal Toggle
+const login = document.getElementById('login')
+const loginModal = document.getElementById('login-modal')
+const closeModal = document.getElementById('cross')
+
+login.addEventListener('click', () => {
+  loginModal.classList.toggle('hide-modal')
+})
+
+closeModal.addEventListener('click', () => {
+  loginModal.classList.toggle('hide-modal')
 })
