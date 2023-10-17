049057f7a9e9cb012afd7ede8cf458bd05627a96
musaHaruna <124810379+musaHaruna@users.noreply.github.com>
2023-10-17 10:17:35 -0700

    add-dropdown-harmbuger-menu-animation

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const toggleButton = document.querySelector('.hamburger-icon')
 const menu = document.getElementById('menu')
 
 toggleButton.addEventListener('click', () => {
-  menu.classList.toggle('hidden')
+  menu.classList.toggle('active')
 })
 
 // Modal Toggle
